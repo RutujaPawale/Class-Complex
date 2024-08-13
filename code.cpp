1dc8b960e6ae3402f6649eaaefb62034b5104559
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-14 00:25:11 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -14,3 +14,11 @@ Complex::Complex(){         // Default Constructor
 real = 0;
 img = 0;
 }
+istream & operator >> (istream &, Complex & i){
+cin >> i.real >> i.img;
+return cin;
+}
+ostream & operator << (ostream &, const Complex & d){
+cout << d.real << " + " << d.img << "i" << endl;
+return cout;
+}
