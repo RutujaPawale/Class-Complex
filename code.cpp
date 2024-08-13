93b0fb8a9e738351bf3305fdfd17a70dc493cb41
Rutuja Pawale <160813855+RutujaPawale@users.noreply.github.com>
2024-08-13 12:00:37 +0530

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,3 +1,13 @@
 # include<iostream>
 using namespace std;
 class Complex                       //decaring Class Complex
+ {
+ double real;       
+ double img;
+ public: 
+ Complex();            // Default Constructor
+ friend istream & operator >> (istream &, Complex &);
+ friend ostream & operator << (ostream &, const Complex &);
+ Complex operator + (Complex); // Addition
+ Complex operator * (Complex); // Multiplication
+ };
